1e245ea89daeb09956d4fbf88c83c13ab6330bab
Artem <Palorel@yandex.ru>
2023-02-19 10:54:32 +0300

    Изменения по ревью

--- sortingQuadraticComplexity/sortingQuadraticComplexity/main.cs
+++ sortingQuadraticComplexity/sortingQuadraticComplexity/main.cs
@@ -1,71 +1,79 @@
 ﻿using System;
+namespace Sort;
 
-namespace Sort
+class Program
 {
-    class Program
+    //Sorts an array using the bubble method
+    public static void BubbleSort(int[] arrayOut)
     {
-        public static void bubbleSort(int[] arrayOut)
+        for (int i = 0; i < arrayOut.Length; ++i)
         {
-            for (int i = 0; i < arrayOut.Length; ++i)
+            for (int j = arrayOut.Length - 1; j > i; --j)
             {
-                for (int j = arrayOut.Length - 1; j > i; --j)
+                if (arrayOut[j - 1] > arrayOut[j])
                 {
-                    if (arrayOut[j - 1] > arrayOut[j])
-                    {
-                        arrayOut[j - 1] = arrayOut[j] ^ arrayOut[j - 1];
-                        arrayOut[j] = arrayOut[j] ^ arrayOut[j - 1];
-                        arrayOut[j - 1] = arrayOut[j] ^ arrayOut[j - 1];
-                    }
+                    (arrayOut[j - 1], arrayOut[j]) = (arrayOut[j], arrayOut[j - 1]);
                 }
             }
         }
+    }
 
-        public static bool testSort()
+    //Test to check the correctness of bubble sorting
+    public static bool testSort()
+    {
+        int[] arrayOut = { 5, 4, 2, 3, 1 };
+        BubbleSort(arrayOut);
+        for (int i = 1; i < 5; ++i)
         {
-            int[] arrayOut = { 5, 4, 2, 3, 1 };
-            bubbleSort(arrayOut);
-            for (int i = 1; i < 5; ++i)
+            if (arrayOut[i] < arrayOut[i - 1])
             {
-                if (arrayOut[i] < arrayOut[i - 1])
-                {
-                    return false;
-                }
+                return false;
             }
-            return true;
         }
-        static void Main(string[] args)
+        return true;
+    }
+
+    static void Main(string[] args)
+    {
+        if (testSort())
         {
-            if (testSort())
-            {
-                Console.WriteLine("Tests correct");
-            }
-            else
-            {
-                Console.WriteLine("Tests failed...");
-                return;
-            }
-            Console.WriteLine("Write size array and elements of array");
-            var inputString = Console.ReadLine();
+            Console.WriteLine("Tests correct");
+        }
+        else
+        {
+            Console.WriteLine("Tests failed...");
+            return;
+        }
+        Console.WriteLine("Write size array and elements of array");
+        var inputString = Console.ReadLine();
+        if (inputString == null)
+        {
+            return;
+        }
+        int sizeArray = 0;
+        bool isCorrectInput = Int32.TryParse(inputString, out sizeArray);
+        if (!isCorrectInput)
+        {
+            return;
+        }
+        var arrayOut = new int[sizeArray];
+        for (int i = 0; i < arrayOut.Length; ++i)
+        {
+            inputString = Console.ReadLine();
             if (inputString == null)
             {
                 return;
             }
-            int sizeArray = Int32.Parse(inputString);
-            int[] arrayOut = new int[sizeArray];
-            for (int i = 0; i < arrayOut.Length; ++i)
+            isCorrectInput = Int32.TryParse(inputString, out arrayOut[i]);
+            if (!isCorrectInput)
             {
-                inputString = Console.ReadLine();
-                if (inputString == null)
-                {
-                    return;
-                }
-                arrayOut[i] = Int32.Parse(inputString);
+                return;
             }
-            bubbleSort(arrayOut);
-            for (int i = 0; i < sizeArray; ++i)
-            {
-                Console.WriteLine(arrayOut[i]);
-            }
+        }
+        BubbleSort(arrayOut);
+        for (int i = 0; i < sizeArray; ++i)
+        {
+            Console.WriteLine(arrayOut[i]);
         }
     }
 }
