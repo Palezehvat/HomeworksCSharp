7c32bd2d889bfaea7df8f886e76511df8ec68eeb
Artem <Palorel@yandex.ru>
2023-03-10 22:20:51 +0300

    Разбиение на файлы

---
+++ stackCalculator/stackCalculator/Tests.cs
@@ -0,0 +1,17 @@
+﻿namespace StackCalculator;
+
+public class Test
+{
+    // Tests the program
+    public bool TestForProgram()
+    {
+        StackCalculator calculator = new StackCalculator();
+        (var isCorrectWork, var result) = calculator.ConvertToAResponse("123 23 +");
+        if (!isCorrectWork || result != 146)
+        {
+            return false;
+        }
+        (isCorrectWork, result) = calculator.ConvertToAResponse("123 23");
+        return !isCorrectWork;
+    }
+}
