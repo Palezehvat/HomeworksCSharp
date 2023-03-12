b539f5494267b46233c10ed537cdd82dbbea98e5
Artem <Palorel@yandex.ru>
2023-03-12 21:05:02 +0300

    Реализация бора

---
+++ Bor/Bor/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Bor;
+
+using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        var bor = new Bor();
+
+        var string1 = "da";
+        var string2 = "db";
+        bool check = bor.Add(string1);
+        if (check == false)
+        {
+            Console.WriteLine("bad news");
+        }
+        check = bor.Add(string2);
+        if (check == false)
+        {
+            Console.WriteLine("bad news");
+        }
+        bor.Remove(string2);
+        Console.WriteLine(bor.HowManyStartsWithPrefix(string1));
+        Console.WriteLine(bor.Contains("da"));
+    }
+}
