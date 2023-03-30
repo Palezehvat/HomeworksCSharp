b2c51d1e1c809a5d49a3e715850bfa754ada9d28
Artem <Palorel@yandex.ru>
2023-03-30 20:41:05 +0300

    Переназванное деление

---
+++ ParsingTree/ParsingTree/Divider.cs
@@ -0,0 +1,26 @@
+﻿namespace ParsingTree;
+
+// A class that implements division
+public class Divider : Operator
+{
+    double delta = 0.0000001;
+
+    // Keeps the division sign in itself
+    public Divider(char symbol) : base(symbol) {}
+
+    // Counts the division of two numbers on each other
+    public override double Calcuate(double firstValue, double secondValue)
+    {
+        if (secondValue - Math.Abs(secondValue) < delta)
+        {
+            throw new ArgumentException();
+        }
+        return firstValue / secondValue;
+    }
+
+    // Displays a division sign with two spaces on the screen
+    public override void Print()
+    {
+        Console.Write(" / ");
+    }
+}
