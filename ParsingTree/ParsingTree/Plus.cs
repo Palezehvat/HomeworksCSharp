1f014e5af71fd3a05a7d53325cd5683fd1ee65b9
Artem <Palorel@yandex.ru>
2023-03-30 20:41:58 +0300

    Забыл класс сложения, добавил

---
+++ ParsingTree/ParsingTree/Plus.cs
@@ -0,0 +1,20 @@
+﻿namespace ParsingTree;
+
+// A class that implements amount
+public class Plus : Operator
+{
+    // Keeps a plus in itself
+    public Plus(char symbol) : base(symbol) {}
+
+    // Counts the Amount of two numbers
+    public override double Calcuate(double firstValue, double secondValue)
+    {
+        return secondValue + firstValue;
+    }
+
+    // Prints a plus sign with spaces
+    public override void Print()
+    {
+        Console.Write(" + ");
+    }
+}
