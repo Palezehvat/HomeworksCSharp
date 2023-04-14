83ba31eaaba283e241cef7f748972de61c2daf50
Artem <Palorel@yandex.ru>
2023-04-14 14:54:45 +0300

    Ненужная информация

--- FirstKrFirstTry/FirstKrFirstTry/Program.cs
+++ FirstKrFirstTry/FirstKrFirstTry/Program.cs
@@ -4,12 +4,6 @@ public class Program
 {
     public static void Main()
     {
-        Queue root = new Queue();
-        root.Enqueue(1, 2);
-        root.Enqueue(2, 3);
-        root.Enqueue(3, 4);
-        root.Enqueue(4, 4);
-        root.Enqueue(5, 4);
-        root.Enqueue(6, 4);
+
     }
 }
